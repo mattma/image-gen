d7537f0fcacec7d4e1babcb691a03d9657e723eb
Matt Ma <hi@mattma.io>
2024-11-02 14:27:48 -0700

    feat: add react-compiler, setup react-compiler lint rule. add prettier formatter, and reset the homepage

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  semi: false,
+  jsxSingleQuote: false,
+  endOfLine: 'lf',
+}
+
+export default config
